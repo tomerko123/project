07a369daf10473656f2fb6ea8ed84c13706d2082
sannyTelyas <sanny4454@gmail.com>
2022-09-01 01:21:42 +0300

    add jsonFile map

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react'
+import React, { useEffect , useState } from 'react'
+import jsonFile2 from "../../json/jsonFile2.json";
+import axios from "axios"; 
 
-import "../../json/jsonFile2.json";
 
 function thisPage1() {
-    const testJson = require("../../json/jsonFile2.json"); 
+    // const [testJson , setTestJson] = React.useState([]); 
+    /*  
+    useEffect(() => {
+        axios
+        .get("../../json/jsonFile2.json")
+        .then((res) => setTestJson(res.data)) 
+        .catch(err => console.log(err)) 
+    } , []); 
+    */
+    // const testJson = require("../../json/jsonFile2.json"); 
     return(
         
-        <div>
-            
-            <div>על המועדון: {testJson[0].About}</div>
-            <div>על מנהל המועדון: {testJson[0].AboutManger}</div>
-
+        <div> 
+            {jsonFile2 &&
+        jsonFile2?.map(({ Instagram ,  Facebook , AboutManger ,  Manager ,  About , Small ,Phone , Email , Address , ClubN, Id }) => (
+          <div key={Id}>
+            <div>{ClubN}</div>
+            <div>{Address}</div>
+            <div>{Small}</div>
+            <div>{Phone}</div>
+            <div>{Email}</div>
+            <div>{About}</div>
+            <div>{Manager}</div>
+            <div>{AboutManger}</div>
+            <div>{Facebook}</div>
+            <div>{Instagram}</div>
+          </div>
+        ))}
         </div>
-        
-        
     ); 
-
-    
 }
 
 
